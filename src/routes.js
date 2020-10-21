a002c95ebf3e386a47ea13ddbf4c55351a42e353
Lee Bao <leebao101@gmail.com>
2020-10-22 00:33:39 +0700

    private route

--- src/routes.js
+++ src/routes.js
@@ -5,28 +5,46 @@ import Signup from './Components/login/signup';
 import Seminar from './pages/seminar';
 import Users from './pages/users';
 import authContext from './contexts/auth/auth-context';
+
+function PrivateRoute({ component: Component, ...rest }) {
+  const [authState, _] = useContext(authContext);
+  const { token, role } = authState;
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        token ? <Component {...props} /> : <Redirect to='/login' />
+      }
+    />
+  );
+}
+
 function Routes(props) {
   const [authState, _] = useContext(authContext);
   const { token, role } = authState;
-
   return (
     <Switch>
-      <Route path='/user-management'>
+      {!token && <Route path='/login' exact component={LoginForm} />}
+      {!token && <Route path='/sign-up' exact component={Signup} />}
+      <PrivateRoute path='/user-management' component={Users} />
+      {/* <Route path='/user-management'>
         {token ? <Users /> : <Redirect to='/login' />}
-      </Route>
+      </Route> */}
+      <PrivateRoute path='/seminar-management' component={Seminar} />
 
-      <Route path='/seminar-management'>
+      {/* <Route path='/seminar-management'>
         {token ? <Seminar /> : <Redirect to='/login' />}
-      </Route>
+      </Route> */}
 
-      <Route path='/login' exact>
+      {/* <Route path='/login' exact>
         {token ? <Redirect to='/' /> : <LoginForm />}
-      </Route>
-      <Route path='/sign-up' exact>
+      </Route> */}
+      {/* <Route path='/sign-up' exact>
         {token ? <Redirect to='/' /> : <Signup />}
       </Route>
-      {!token && <Redirect to='/login' />}
-      <Route path='/' exact component={Seminar} />
+      {!token && <Redirect to='/login' />} */}
+      {/* <Route path='/' exact component={Seminar} /> */}
+      <PrivateRoute path='/' component={Seminar} />
     </Switch>
   );
 }
